5c5353e19f45b26fd27fe8a0cb97a393e35fa8d4
jyothi2795 <jyothi271195@gmail.com>
2016-09-19 21:32:15 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Compare {
+
+    public static void main(String[] args) { 
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        if(number == int) 
+ {           System.out.println("Number is natural and positive.");
+          
+}
+else if (number < 0) 
+    System.out.print("number is negative");
+    }
+}
